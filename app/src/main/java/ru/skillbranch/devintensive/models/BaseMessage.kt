0a5885029bab5981f9ace8b73dba38a7bfc8c843
Crone163 <defrag163@mail.ru>
2019-09-08 03:24:31 +0400

    hometask_5 ???

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -7,12 +7,35 @@ import java.util.*
 /**
  * Created by Makweb on 24.06.2019.
  */
+
+
 abstract class BaseMessage(
     val id: String,
     val from: User,
     val chat: Chat,
-    val isIncoming: Boolean = true,
+    val isIncoming: Boolean = false,
     val date: Date = Date(),
     var isReaded: Boolean = false
+) {
+    abstract fun formatMessage(): String
 
-)
+    companion object AbstractFactory {
+        var lastId: Int = -1
+
+        fun makeMessage(
+            from: User,
+            chat: Chat,
+            date: Date = Date(),
+            type: String = "text",
+            payload: Any?,
+            isIncoming: Boolean = false
+        ): BaseMessage {
+            lastId++
+            return when (type) {
+                "image" -> ImageMessage("$lastId", from, chat, isIncoming, date, image = payload as String)
+                else -> TextMessage("$lastId", from, chat, isIncoming, date, text = payload as String)
+            }
+        }
+
+    }
+}
